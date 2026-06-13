55603518053f7c7b6e3c6e6b4b08b9788f030163
AndiPardede <andipardede473@gmail.com>
2026-06-13 11:15:35 +0700

    Add files via upload

---
+++ PERTEMUAN 1/Test 1.sql
@@ -0,0 +1,18 @@
+-- CREATE DATABASE db_ecommerce;
+-- USE db_ecommerce;	
+-- CREATE TABLE pelanggan(
+-- id_plg INT PRIMARY KEY auto_increment, 
+-- nama varchar(255), 
+-- email varchar (255) UNIQUE, 
+-- telepon VARCHAR (255), 
+-- alamat TEXT
+-- );
+
+-- insert into pelanggan (nama, email, telepon, alamat) values
+-- ('ANDI','testing@gmail.com1','081238912','Menteng 1'),
+-- ('AKBAR','testing@gmail.com2','081238913','Menteng 2'),
+-- ('NENA','testing@gmail.com3','081238914','Menteng 3'),
+-- ('ADIT','testing@gmail.com4','081238915','Menteng 4'),
+-- ('CASUR','testing@gmail.com5','081238916','Menteng 5');
+
+SELECT * FROM Pelanggan;
